8a9fe317dd922085846a63f67d05716d3dd4c838
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-12 22:20:43 +0900

    Added useNotification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import {useFullscreen} from "./useFullscreen";
+import { useNotification } from "./useNotification";
 
 function App() {
-    const onFullScreen = isFull => {
-        console.log(isFull ? "true" : "false");
-    }
-    const { element, triggerFull, exitFull } = useFullscreen(onFullScreen);
+    const triggerNotif = useNotification("test", {body: "This is test"});
     return (
         <>
-            <div ref={element}>
-                <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="test"/>
-                <button onClick={exitFull}>exit screen</button>
-            </div>
-            <button onClick={triggerFull}>full screen</button>
+            <button onClick={triggerNotif}>test</button>
         </>
     );
 }
